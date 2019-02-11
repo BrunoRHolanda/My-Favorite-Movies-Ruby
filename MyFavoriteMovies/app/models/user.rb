be3ad1adf6e1463b9993d37cf8c70df8cbe828b2
Bruno R. Holanda <brunorodriguesholanda@gmail.com>
2019-02-11 05:18:22 +0000

    Adicionado gems para auth com token

--- MyFavoriteMovies/app/models/user.rb
+++ MyFavoriteMovies/app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+    acts_as_token_authenticatable
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
+
     validates :name, :email, :password, :birthday, presence: { message: "Campo obrigatório !" } 
     validates :email, 
         uniqueness: {
--- MyFavoriteMovies/db/schema.rb
+++ MyFavoriteMovies/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_013042) do
+ActiveRecord::Schema.define(version: 2019_02_11_045751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2019_02_11_013042) do
     t.date "birthday"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "authentication_token", limit: 30
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
   end
 
   add_foreign_key "movies", "profiles", column: "profiles_id"
