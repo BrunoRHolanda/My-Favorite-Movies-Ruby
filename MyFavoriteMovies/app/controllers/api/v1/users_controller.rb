4bbaac2d22e72b5f95ea2475e1ef268a144abb02
Bruno R. Holanda <brunorodriguesholanda@gmail.com>
2019-02-11 04:30:05 +0000

    finalizado manager user

--- MyFavoriteMovies/app/controllers/api/v1/users_controller.rb
+++ MyFavoriteMovies/app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,23 @@
 class Api::V1::UsersController < ApplicationController
     def index
-        @users = User.all
-        render json: users, status: ok
+        users = User.all
+        render json: users, status: :ok
+    end
+
+    def show
+        user = User.find( params[:id] )
+        render json: user, status: :ok
+    end
+
+    def create 
+        user = User.new( user_params )
+        render json: { errors: user.errors }, status: :unprocessable_entity if user.invalid?
+        render json: user, status: :ok if user.save
+    end
+
+    private
+
+    def user_params
+        params.permit( :name, :email, :password, :birthday )
     end
 end
--- MyFavoriteMovies/app/models/user.rb
+++ MyFavoriteMovies/app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
-    validates :name, :email, :password, :birthday, presence: true
-    validates :email, uniqueness: true
+    validates :name, :email, :password, :birthday, presence: { message: "Campo obrigatório !" } 
+    validates :email, 
+        uniqueness: {
+            message: ->(object, data) do
+                "Olá #{object.name}!, #{data[:value]} já está cadastrado! Tente novamente com um email diferente. #{Time.zone.tomorrow}"
+              end
+        }
 end
