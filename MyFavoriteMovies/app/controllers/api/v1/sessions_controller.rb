e9b12ac1835f1ed73cc6dddee6c6455a7020d3b2
Bruno R. Holanda <brunorodriguesholanda@gmail.com>
2019-02-11 20:04:48 +0000

    :)

--- MyFavoriteMovies/app/controllers/api/v1/sessions_controller.rb
+++ MyFavoriteMovies/app/controllers/api/v1/sessions_controller.rb
@@ -1,28 +1,28 @@
 class Api::V1::SessionsController < ApplicationController
 
     def index
-        head(:method_not_allowed)
+        head( :method_not_allowed )
     end
 
     def create 
-        user = User.where(email: params[:email]).first
+        user = User.where( email: params[ :email ]).first
 
-        if user&.valid_password?(params[:password])
-            render json: user.as_json(only: [:email, :authentication_token]), status: :created
+        if user&.valid_password?( params[:password ])
+            render json: user.as_json( only: [ :email, :authentication_token ]), status: :created
         else
-            head(:unauthorized)
+            head( :unauthorized )
         end
     end
 
     def show
-        head(:method_not_allowed)
+        head( :method_not_allowed )
     end
 
     def update
-        head(:method_not_allowed)
+        head( :method_not_allowed )
     end
 
     def destroy
-        head(:method_not_allowed)
+        head( :method_not_allowed )
     end
 end
