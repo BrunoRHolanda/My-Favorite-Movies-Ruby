3cd89cc2695d217b4d55c6c8175783924f65a6e9
Bruno R. Holanda <brunorodriguesholanda@gmail.com>
2019-02-11 03:29:33 +0000

    Criado controller user e action index

--- MyFavoriteMovies/app/controllers/api/v1/user_controller.rb
+++ MyFavoriteMovies/app/controllers/api/v1/user_controller.rb
@@ -1,4 +1,4 @@
-class UserController < ApplicationController
+class Api::V1::UserController < ApplicationController
     def index
         @users = Users.all
         render json: users, status: ok
